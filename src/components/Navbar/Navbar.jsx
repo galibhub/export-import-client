5a1000daa80bee1a447a78c1762eb62476293752
Ibrahim Ahmed Galib <galib@Ibrahims-Mac-mini.local>
2025-11-18 12:54:29 +0600

    Conditional Rendering on SignIn and SignOut button on Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,38 @@
+import { use } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const Navbar = () => {
+  const { user } = use(AuthContext);
   const links = (
     <>
-      <li className="text-primary font-semibold" ><NavLink to="/">Home</NavLink></li>
-      <li className="text-primary font-semibold"> <NavLink to="/allProducts">All Products</NavLink> </li>
-      <li className="text-primary font-semibold"> <NavLink to="/myExport">My Export</NavLink> </li>
-      <li className="text-primary font-semibold"> <NavLink to="/myImport">My Import</NavLink> </li>
-      <li className="text-primary font-semibold"> <NavLink to="/addExport">Add Export</NavLink> </li>
-      <li className="text-primary font-semibold"> <NavLink to="/login">Login</NavLink> </li>
-      <li className="text-primary font-semibold"> <NavLink to="/Register">Register</NavLink> </li>
+      <li className="text-primary font-semibold">
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li className="text-primary font-semibold">
+        {" "}
+        <NavLink to="/allProducts">All Products</NavLink>{" "}
+      </li>
+      <li className="text-primary font-semibold">
+        {" "}
+        <NavLink to="/myExport">My Export</NavLink>{" "}
+      </li>
+      <li className="text-primary font-semibold">
+        {" "}
+        <NavLink to="/myImport">My Import</NavLink>{" "}
+      </li>
+      <li className="text-primary font-semibold">
+        {" "}
+        <NavLink to="/addExport">Add Export</NavLink>{" "}
+      </li>
+      <li className="text-primary font-semibold">
+        {" "}
+        <NavLink to="/login">Login</NavLink>{" "}
+      </li>
+      <li className="text-primary font-semibold">
+        {" "}
+        <NavLink to="/Register">Register</NavLink>{" "}
+      </li>
     </>
   );
   return (
@@ -40,14 +63,29 @@ const Navbar = () => {
             {links}
           </ul>
         </div>
-        <NavLink to='/' className="btn btn-ghost text-xl">Export <span className="text-primary">Import</span> Hub</NavLink>
+        <NavLink to="/" className="btn btn-ghost text-xl">
+          Export <span className="text-primary">Import</span> Hub
+        </NavLink>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
       <div className="navbar-end">
-        
-        <input type="checkbox" defaultChecked className="toggle toggle-primary" />
+        <input
+          type="checkbox"
+          defaultChecked
+          className="toggle toggle-primary"
+        />
+        <div className="flex items-center gap-2">
+          <span className="text-sm font-medium">{user && user.email}</span>
+          {user ? (
+            <button className="btn btn-primary">Sign Out</button>
+          ) : (
+            <NavLink to="/login">
+              <button className="btn btn-primary">Login</button>
+            </NavLink>
+          )}
+        </div>
       </div>
     </div>
   );
